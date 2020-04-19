a8d762c8b14b2c0ed468d0b50e3d60187e805af8
dante18 <lablandinerie2@gmail.com>
2020-04-19 16:33:55 +0200

    Récupération et stockage des données dans $_SESSION

--- index.php
+++ index.php
@@ -1,4 +1,35 @@
-<?php require 'inc/head.php'; ?>
+<?php
+require 'inc/head.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    if (isset($_GET['add_to_cart'])) {
+        if(empty($_SESSION['card']['product'])) {
+            $_SESSION['card']['product'] = [];
+        }
+
+        switch ($_GET["add_to_cart"]) {
+            case "46":
+                array_push($_SESSION['card']['product'], "pecanNuts");
+                header('Location: index.php');
+                break;
+            case "36":
+                array_push($_SESSION['card']['product'], "chocolateChips");
+                header('Location: index.php');
+                break;
+
+            case "58":
+                array_push($_SESSION['card']['product'], "chocolateCookie");
+                header('Location: index.php');
+                break;
+
+            case "32":
+                array_push($_SESSION['card']['product'], "mmsCookies");
+                header('Location: index.php');
+                break;
+        }
+    }
+}
+?>
 <section class="cookies container-fluid">
   <div class="row">
     <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
